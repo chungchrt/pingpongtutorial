804d4f279227c4b944edbf207fff3d1a1283ec58
chungchrt <chungchung20022003@hotmail.com>
2020-09-02 17:21:58 +0800

    Added Photon
    已加入photon連線

---
+++ pingpong tutorial/Assets/Script/mainmenu/Mainmenu.cs
@@ -0,0 +1,88 @@
+﻿using JetBrains.Annotations;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject findOpponentPanel = null;
+    [SerializeField] private GameObject waitingStatusPanel = null;
+    [SerializeField] private TextMeshProUGUI waitingStatusText = null;
+
+    private bool isConnecting = false;
+    private const string GameVersion = "0.1"; //Pascal naming for const 
+    private const int MaxPlayersPerRoom = 2;
+
+    private void Awake() => PhotonNetwork.AutomaticallySyncScene = true; // sync the scene between the players
+
+    public void FindOpponent()
+    {
+        isConnecting = true; // when finding oppoenent its connected
+        findOpponentPanel.SetActive(false); //close the find opponent panel
+        waitingStatusPanel.SetActive(true);//enable waiting status panel
+        waitingStatusText.text = "Searching......";//change the text to seraching..\
+
+        if (PhotonNetwork.IsConnected) //if its connected (PN api)
+        {
+            PhotonNetwork.JoinRandomRoom(); //Joing a room PN function
+        }
+        else //if its not connected , assign gameversion then connect
+        {
+            PhotonNetwork.GameVersion = GameVersion; //Photon's GameVersion assign to Gameversion
+            PhotonNetwork.ConnectUsingSettings(); //connect with photon setting
+        }
+    }
+        
+        public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        waitingStatusPanel.SetActive(false);
+        findOpponentPanel.SetActive(true);
+
+        Debug.Log($"Disconnected due to : {cause}");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No clients are waiting for an opponent , creating a new room"); //If join random room failed > create a room
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom }); //null = name , roomoptions list > set the max player
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client successfully joined a room");
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount; // Count the player
+        if(playerCount != MaxPlayersPerRoom) // if the player count is not equal to Maxplayerperroom 
+        {
+            waitingStatusText.text = "Waiting for Opponent";
+            Debug.Log("Player is waiting for an oppoent");
+        }
+        else // player count = MaxPlayerPerRoom
+        {
+            waitingStatusText.text = "Opponent Found";
+            Debug.Log("Matching is ready to begin");
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false; //if playercount == MaxPlayer then close the room 
+            waitingStatusText.text = "Opponent Found";
+            Debug.Log("Match is ready to begin");
+
+            PhotonNetwork.LoadLevel("pingpong");
+        }
+    }
+}
+
---
+++ pingpong tutorial/Assets/Script/mainmenu/PlayerNameInput.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] private InputField nameInputField = null; //declare nameinput
+    [SerializeField] private Button continueButton = null; // declare button 
+
+    private const string PlayerPrefsNameKey = "PlayerName"; // constant name => second login = same name . Default：player name.
+
+    private void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; } // If PlayerPrefs(local storage ) do not have name => return
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey); // if have name, get the local storage name ( saved name)
+
+        nameInputField.text = defaultName; //set inputfield to player name 
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name); //the button is enable if the name is not Empty
+    }
+
+    public void SavePlayerName()
+    {
+        String playerName = nameInputField.text; //get playername from text
+
+        PhotonNetwork.NickName = playerName; // photon name = name
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName); // save name to local storage
+    }
+}
